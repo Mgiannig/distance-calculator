73464da9bf86cb1013f198b4e6f07e4e11bdebf8
Mariano Gianni <Mariano_Gianni@hotmail.com>
2022-08-29 22:38:48 -0300

    moved google api key to env

--- src/googleApi.ts
+++ src/googleApi.ts
@@ -2,7 +2,7 @@ import { LatLngLiteral } from "@googlemaps/google-maps-services-js";
 import axios from "axios";
 import { DistanceMatrixApiResponse } from "./model/distance-matrix-api-response";
 
-const apikey = process.env.GOOGLE_API_KEY || "AIzaSyARrcDNt2Yks1eeSmPMNSRfJrQLnnaC81I";
+const apikey = process.env.GOOGLE_API_KEY;
 
 export async function getDistance(origin: LatLngLiteral, destination: LatLngLiteral): Promise<DistanceMatrixApiResponse> {
     const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.lat},${origin.lng}&destinations=${destination.lat},${destination.lng}&key=´${apikey}`
